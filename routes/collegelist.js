cb3b1457b37f1e752d137cfcd55993a1ddda91ea
Keelyh <keely.haverstock@students.olin.edu>
2014-01-17 11:34:12 -0800

    fixing the bug that used schoollist -- i removed that model

--- routes/collegelist.js
+++ routes/collegelist.js
@@ -2,8 +2,6 @@ var models = require("../models.js");
 
 exports.disp = function(req, res){
   models.School.find({}).exec(function (err, schools){
-    models.SchoolList.find({}).exec(function (err, schoolList){
-      res.render('collegeList', { title: 'College List', user:req.session.user, schools:schools});
-    })
+    res.render('collegeList', { title: 'College List', user:req.session.user, schools:schools});
   })
 }
